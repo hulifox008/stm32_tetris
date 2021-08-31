174abe7f4958f5a03f13bac0589f1f41d5fdbbb8
Fox <hulifox008@gmail.com>
2021-08-31 11:45:08 -0500

    GPIO config.
    Signed-off-by: Fox <hulifox008@gmail.com>

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -277,7 +277,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, SPI1_CS_Pin|DISP_LED_Pin|DISP_RST_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, SPI1_CS_Pin|DISP_LED_Pin|DISP_RST_Pin|DISP_D_Cn_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : SW_IN_Pin */
   GPIO_InitStruct.Pin = SW_IN_Pin;
@@ -285,8 +285,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(SW_IN_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : SPI1_CS_Pin DISP_LED_Pin DISP_RST_Pin */
-  GPIO_InitStruct.Pin = SPI1_CS_Pin|DISP_LED_Pin|DISP_RST_Pin;
+  /*Configure GPIO pins : SPI1_CS_Pin DISP_LED_Pin DISP_RST_Pin DISP_D_Cn_Pin */
+  GPIO_InitStruct.Pin = SPI1_CS_Pin|DISP_LED_Pin|DISP_RST_Pin|DISP_D_Cn_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
